a2645fc2782203d4de16580b119d9c9f5bfbdb02
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2017-06-05 21:07:40 +0200

    Made a boo boo in my spec :(

--- day_9/src/part_b.rs
+++ day_9/src/part_b.rs
@@ -1,13 +1,13 @@
 use regex::Regex;
+use std::str::FromStr;
 
 pub fn solve(data: &str) {
-
-
     println!("{}", "------------- [ PART B ] -------------");
     println!("NOOT NOOT! its da sound of da pingu 🐧");
     println!("{}", "--------------------------------------");
 }
 
+#[derive(Debug)]
 pub struct Node {
     content_length: usize,
     multiplier: usize,
@@ -17,6 +17,21 @@ pub struct Node {
 impl Node {
     pub fn len(&self) -> usize {
         self.multiplier * (self.content_length + self.nodes.iter().fold(0, {|v, n| v + n.len() }))
+    }
+}
+
+impl FromStr for Node {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+
+        let res = Node {
+            content_length: 10,
+            multiplier: 1,
+            nodes: Vec::new(),
+        };
+
+        Ok(res)
+
     }
 }
 
@@ -48,18 +63,19 @@ mod node_tests {
         assert_eq!(subject.len(), 1320);
     }
 
+    #[test]
     fn test_solve_with_multi_level_subnodes() {
-        // example: (answer should be 5220)
-        // (127x2)                          => (1098 + 140 + 1372) * 2 = 5220
+        // example: (answer should be 5148)
+        // (127x2)                          => (1098 + 140 + 1336) * 2 = 5148
         //      (41x6)                      => (144 + 39) * 6 = 1098
         //          (16x9)SIUZCKMFZFXKUYTQ  => 144
         //          (13x3)YBCVHJPPFAONV     => 39
         //      (10x14)BTRWBQRUHA           => 10 * 14 = 140
-        //      (57x4)                      => (180 + 55 + 72 + 36) * 4 = 1372
+        //      (57x4)                      => (180 + 55 + 72 + 27) * 4 = 1336
         //          (12x15)ZUMPYOEOOBFW     => 180
         //          (5x11)YNLIJ             => 55
         //          (8x9)GBQFPTOH           => 72
-        //          (9x3)GPFCSAPZD          => 36
+        //          (9x3)GPFCSAPZD          => 27
 
         let subject = Node {
             content_length: 0,
@@ -93,7 +109,13 @@ mod node_tests {
             ]
         };
 
-        assert_eq!(subject.len(), 5220);
+        assert_eq!(subject.len(), 5148);
+    }
 
+    #[test]
+    fn test_from_str_simple() {
+        let node : Node = "(5x11)ABCDE".parse().unwrap();
+
+        println!("{:?}", node);
     }
 }
